9c4c616fa9ca2788012ffd6a74443a41302b1f86
Quyen Bui <quyen@go1.com.au>
2016-08-11 13:43:08 +0700

    Add mock

---
+++ mocks/mock.sh
@@ -0,0 +1,3 @@
+
+daemon --command="json-server --watch --read-only /home/quyen/OwnProjects/api-exploder/mocks/account-resource.json" --output=/tmp/account-resource.json.log
+daemon --command="json-server --watch /home/quyen/OwnProjects/api-exploder/mocks/account-data.json" --output=/tmp/account-data.json.log
